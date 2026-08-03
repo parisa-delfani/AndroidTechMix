ad35846cd31cd594385dfd4c46f03c98844d107e
Parinexus <Parinexus@gmail.com>
2026-08-03 16:43:59 +0330

    chore: remove legacy weather data and domain modules
    Clear the previous AccuWeather/OpenWeather scaffold to make room for the GitHub Users architecture.

--- domain/src/main/java/android/tech/mix/domain/model/Theme.kt
+++
@@ -1,6 +0,0 @@
-package android.tech.mix.domain.model
-
-enum class Theme {
-    LIGHT_THEME,
-    DARK_THEME
-}
--- domain/src/main/java/android/tech/mix/domain/model/WindSpeedUnit.kt
+++
@@ -1,7 +0,0 @@
-package android.tech.mix.domain.model
-
-enum class WindSpeedUnit {
-    METERS_PER_SECOND,
-    KILOMETERS_PER_HOUR,
-    MILES_PER_HOUR
-}
